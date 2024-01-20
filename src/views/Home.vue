2dbe9c003d9a3f2966ada3c2600b17c4307a6079
Mario Prieta Sánchez <145470491+mariops03@users.noreply.github.com>
2024-01-21 00:04:48 +0100

    feature(home page)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,19 @@
 <template>
-
+    <h1 class="text-4xl font-bold p-6">Interesting recipes</h1>
+    <Meals :meals="comidas"/>
 </template>
 
 <script setup>
+    import { onMounted, ref } from "vue";
+    import axiosClient from "../axiosClient";
+    import Meals from "../components/Meals.vue";
+    const comidas = ref([]);
 
+    onMounted(()=>{
+        for(let i = 0; i < 10; i++){
+            axiosClient.get('/random.php').then(({data})=>{
+                comidas.value.push(data.meals[0]);
+            })
+        }
+    });
 </script>
